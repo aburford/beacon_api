7b57f8d4d9f8cb9c0e87f5f2d33bcb407caa5a7e
Andrew Burford <andrew@burford.cf>
2018-05-26 23:12:06 -0400

    get auth_token mostly working

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -5,15 +5,41 @@ class ApiController < ApplicationController
   	pass_correct = true
   	if pass_correct
   		puts "Creating new user..."
-  		s = Student.new(username: params['user'])
-  		s.save
-  		render json: s
+  		render json: Student.create(username: params['user'])
   	end
   end
 
   def hashes
-  	authenticate
-  	return plain "success"
+  	if authenticate
+	  	render plain: %(
+[
+  {
+    "period": 2,
+    "date": "2018-05-25",
+    "hash": "1a48fa063cff47efaf1f011e23d4e6b0",
+    "attendance_code": 1
+  },
+  {
+    "period": 2,
+    "date": "2018-05-25",
+    "hash": "16596d62153747a58350660b3fa0a872",
+    "attendance_code": 2
+  },
+  {
+    "period": 3,
+    "date": "2018-05-25",
+    "hash": "7c6195bb942a4c719ee96a4fb94f6788",
+    "attendance_code": 1
+  },
+  {
+    "period": 3,
+    "date": "2018-05-25",
+    "hash": "34b8c621e8e64c72991d2144db6c7be2",
+    "attendance_code": 2
+  }
+]
+	  	)
+    end
   end
 
   def present
@@ -21,7 +47,10 @@ class ApiController < ApplicationController
   end
 
   def authenticate
+  	puts "authenticate method called"
   	authenticate_or_request_with_http_token do |token, options|
+  		puts "token:"
+  		puts token
       Student.find_by(auth_token: token)
     end
   end
